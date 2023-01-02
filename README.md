03a5fb7a6d604796c17407b67db334ef49bed59d
cml1010101 <cml1010101@gmail.com>
2023-01-01 21:18:00 -0500

    updated to 2023

--- README.md
+++
@@ -1,2 +0,0 @@
-# ChargedUp
-This is where the code for the new robot will go.
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,32 +5,31 @@
 package frc.robot;
 
 import frc.robot.commands.DriveWithJoystick;
-import frc.robot.subsystems.CameraMount;
 import frc.robot.subsystems.Drivetrain;
-import frc.robot.subsystems.TrackingSystem;
-import frc.robot.subsystems.TrackingSystem.CameraFilter;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   public static final Drivetrain drivetrain = new Drivetrain();
-  public static final CameraMount cameraMount = new CameraMount();
-  public static final TrackingSystem trackingSystem = new TrackingSystem(Constants.CAMERA_ID,
-   CameraFilter.APRILTAG);
-  SendableChooser<Command> autonomousChooser;
+  private final SendableChooser<Command> autonomousChooser;
 
   private final OI oi = new OI();
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
     initDefaultCommands();
     oi.bindButtons();
