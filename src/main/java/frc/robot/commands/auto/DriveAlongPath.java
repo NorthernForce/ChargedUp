fd025bc7406b4ea4d552181254bdc72377c3fd41
cml1010101 <cml101010@gmail.com>
2023-01-12 10:16:37 -0500

    added DriveAlongPath

---
+++ src/main/java/frc/robot/commands/auto/DriveAlongPath.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.auto;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.RamseteController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
+import frc.robot.Constants;
+
+import static frc.robot.RobotContainer.*;
+
+import java.util.List;
+
+public class DriveAlongPath extends RamseteCommand {
+  /** Creates a new DriveAlongPath. */
+  public DriveAlongPath(List<Translation2d> waypoints, Pose2d newLocation) {
+    super(
+      TrajectoryGenerator.generateTrajectory(
+        positioningSubsystem.estimatePose(),
+        waypoints,
+        newLocation,
+        new TrajectoryConfig(Constants.maxSpeed, Constants.maxAcceleration)),
+      positioningSubsystem::estimatePose,
+      new RamseteController(),
+      new SimpleMotorFeedforward(Constants.kS, Constants.kV, Constants.kA),
+      new DifferentialDriveKinematics(Constants.trackWidth),
+      drivetrain::getSpeeds,
+      new PIDController(Constants.driveP, 0, 0),
+      new PIDController(Constants.driveP, 0, 0),
+      drivetrain::driveVolts,
+      drivetrain, positioningSubsystem, imu
+    );
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+}
