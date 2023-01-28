f56d7c886f7bdc55f65969c6f056d4414b354031
crandall-172 <christian.p.randall@gmail.com>
2023-01-28 14:18:08 -0500

    led

--- src/main/java/frc/robot/subsystems/Compressor.java
+++ src/main/java/frc/robot/subsystems/Compressor.java
@@ -5,12 +5,21 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+import edu.wpi.first.wpilibj.Relay;
 public class Compressor extends SubsystemBase {
   /** Creates a new Compressor. */
   public Compressor() {}
 
-  @Override
+// Turns on/off compressor.
+
+
+public double getPressure(){
+  return 0;
+
+}
+//Checks compressor pressure.
+
+@Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
--- src/main/java/frc/robot/subsystems/LED.java
+++ src/main/java/frc/robot/subsystems/LED.java
@@ -5,10 +5,27 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.Relay;
+
 
 public class LED extends SubsystemBase {
+  private final Relay relay = new Relay(0);
+  
   /** Creates a new LED. */
-  public LED() {}
+  public LED() {
+
+
+
+  }
+  // Turns LED on.
+  public void enable(){
+    relay.set(Relay.Value.kForward);
+  
+  }
+  // Turns LED off.
+  public void disable(){
+    relay.set(Relay.Value.kOff);
+  }
 
   @Override
   public void periodic() {
