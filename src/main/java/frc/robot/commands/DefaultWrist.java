698d6a12ea7c27f4b863a8a6bc24b222a972b495
cml1010101 <cml1010101@gmail.com>
2023-02-17 11:59:21 -0500

    forgot to change something

--- src/main/java/frc/robot/commands/DefaultWrist.java
+++ src/main/java/frc/robot/commands/DefaultWrist.java
@@ -21,7 +21,7 @@ public class DefaultWrist extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    wrist.setWristAngle(arm.getAngle().unaryMinus());
+    wrist.setAngle(arm.getAngle().unaryMinus());
   }
 
   // Called once the command ends or is interrupted.
