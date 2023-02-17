639f47aa32eb6e455bdbe46e282b574e3d9753be
cml1010101 <cml1010101@gmail.com>
2023-02-16 22:36:50 -0500

    more changes; got rid of Arm feed forward

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -4,7 +4,6 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
@@ -25,10 +24,108 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 public class Arm extends SubsystemBase {
   // We know we will have two talons
   private final WPI_TalonFX leftMotor, rightMotor;
-  private final WPI_TalonFX extensionMotor1;
-  private final WPI_TalonFX extensionMotor2;
-  private final ArmFeedforward armFeedforward;
-  private final PIDController rotateController, extensionController1, extensionController2;
+  private final WPI_TalonFX extensionMotor;
+  private final PIDController rotateController, extensionController;
+  /** Creates a new Arm. */
+  public Arm() {
+    // Assert required subsystem have been declared
+    leftMotor = new WPI_TalonFX(Constants.ARM_LEFT_MOTOR);
+    rightMotor = new WPI_TalonFX(Constants.ARM_RIGHT_MOTOR);
+    leftMotor.setInverted(TalonFXInvertType.OpposeMaster);
+    leftMotor.follow(rightMotor);
+    configureController(leftMotor, true);
+    configureController(rightMotor, false);
+    rotateController = new PIDController(Constants.ARM_PROPORTION, 0, 0);
+    rotateController.setSetpoint(Constants.ARM_STARTING_ROTATION.getRadians());
+    extensionMotor = new WPI_TalonFX(Constants.ARM_EXTENSION_MOTOR_ID);
+    configureController(extensionMotor, false);
+    extensionController = new PIDController(Constants.ARM_EXTENSION_PROPORTION, 0, 0);
+  }
+  /** Extends Arm */
+  public void extend()
+  {
+    extensionController.setSetpoint(Constants.ARM_EXTENDED_LENGTH);
+  }
+  /** Retracts Arm */
+  public void retract()
+  {
+    extensionController.setSetpoint(Constants.ARM_RETRACTED_LENGTH);
+  }
+  /**
+   * Set length to extend to
+   * @param distance distance meters
+   */
+  public void setArmExtension(double distance)
+  {
+    extensionController.setSetpoint(distance);
+  }
+  /**
+   * Gets the total length of the extended arm
+   * @return total extended arm length
+   */
+  public double getExtendedArmLength()
+  {
+    return (extensionMotor.getSelectedSensorPosition() / 2048) / Constants.EXTENSION_GEAR_RATIO
+      * Constants.EXTENSION_DISTANCE_PER_ROTATION;
+  }
+  /**
+   * Get arm angle
+   * @return Offset from horizon line
+   */
+  public Rotation2d getAngle()
+  {
+    return Rotation2d.fromRotations((rightMotor.getSelectedSensorPosition() / 2048)
+      / Constants.ROTATE_GEAR_RATIO);
+  }
+  /**
+   * Set arm angle
+   * @param angle angle to set the arm to
+  */
+  public void setAngle(Rotation2d angle)
+  {
+    rotateController.setSetpoint(angle.getRadians());
+  }
+  /**
+   * Tells whether the arm is extended
+   * @return whether the arm is extended
+   */
+  public boolean isExtended()
+  {
+    return Math.abs(getExtendedArmLength() - Constants.ARM_EXTENDED_LENGTH) >= 0.05;
+  }
+  /**
+   * Tells whether the arm is retracted
+   * @return whether the arm is retracted
+   */
+  public boolean isRetracted()
+  {
+    return Math.abs(getExtendedArmLength() - Constants.ARM_RETRACTED_LENGTH) >= 0.05;
+  }
+  /**
+   * Set arm angular speed
+   * @param speed between 1.0 and -1.0
+   */
+  public void setArmSpeed(double speed)
+  {
+    rotateController.setSetpoint(rotateController.getSetpoint() + Rotation2d.fromDegrees(speed).getRadians());
+  }
+  /**
+   * Gets the current translation of the end of the arm from the robot orign
+   * @return Translation3d
+   */
+  public Translation3d getArmTranslation()
+  {
+    return Constants.ARM_ORIGIN.plus(new Translation3d(
+      getExtendedArmLength(),
+      new Rotation3d(0, getAngle().getRadians(), 0)
+    ));
+  }
+  @Override
+  public void periodic() {
+    rightMotor.set(rotateController.calculate(getAngle().getRadians()));
+    extensionMotor.set(extensionController.calculate(getExtendedArmLength()));
+    SmartDashboard.putNumber("Arm Angle", arm.getAngle().getDegrees());
+  }
   /**
    * Configures a controller
    * @param controller motor controller
@@ -49,118 +146,5 @@ public class Arm extends SubsystemBase {
     TalonFXConfiguration configs = new TalonFXConfiguration();
     configs.primaryPID.selectedFeedbackSensor = FeedbackDevice.IntegratedSensor;
     controller.configAllSettings(configs);
-  }
-  /** Creates a new Arm. */
-  public Arm() {
-    // Assert required subsystem have been declared
-    assert pcm != null;
-    leftMotor = new WPI_TalonFX(Constants.ARM_LEFT_MOTOR);
-    rightMotor = new WPI_TalonFX(Constants.ARM_RIGHT_MOTOR);
-    leftMotor.setInverted(TalonFXInvertType.OpposeMaster);
-    leftMotor.follow(rightMotor);
-    configureController(leftMotor, true);
-    configureController(rightMotor, false);
-    armFeedforward = new ArmFeedforward(Constants.ARM_KS, Constants.ARM_KG, Constants.ARM_KV,
-      Constants.ARM_KG);
-    rotateController = new PIDController(Constants.ARM_PROPORTION, 0, 0);
-    rotateController.setSetpoint(Constants.ARM_STARTING_ROTATION.getRadians());
-    extensionMotor1 = new WPI_TalonFX(Constants.ARM_EXTENSION_MOTOR_1_ID);
-    extensionMotor2 = new WPI_TalonFX(Constants.ARM_EXTENSION_MOTOR_2_ID);
-    configureController(extensionMotor1, false);
-    configureController(extensionMotor2, false);
-    extensionController1 = new PIDController(Constants.ARM_EXTENSION_PROPORTION, 0, 0);
-    extensionController2 = new PIDController(Constants.ARM_EXTENSION_PROPORTION, 0, 0);
-  }
-  /** Extends Arm */
-  public void extend()
-  {
-    extensionController1.setSetpoint(Constants.ARM_INNER_EXTENDED_LENGTH);
-    extensionController2.setSetpoint(Constants.ARM_OUTER_EXTENDED_LENGTH);
-  }
-  /** Retracts Arm */
-  public void retract()
-  {
-    extensionController1.setSetpoint(Constants.ARM_INNER_RETRACTED_LENGTH);
-    extensionController2.setSetpoint(Constants.ARM_OUTER_RETRACTED_LENGTH);
-  }
-  public void setArmExtension(double distance)
-  {
-    extensionController1.setSetpoint(distance / 2);
-    extensionController2.setSetpoint(distance / 2);
-  }
-  /**
-   * Gets the total length of the extended arm
-   * @return total extended arm length
-   */
-  public double getExtendedArmLength()
-  {
-    return getInnerExtendedArmLength() + getOuterExtendedArmLength();
-  }
-  private double getInnerExtendedArmLength()
-  {
-    return 0;
-  }
-  private double getOuterExtendedArmLength()
-  {
-    return 0;
-  }
-  /**
-   * Get arm angle
-   * @return Offset from horizon line
-   */
-  public Rotation2d getAngle()
-  {
-    return null;
-  }
-  /**
-   * Set arm angle
-   * @param angle angle to set the arm to
-  */
-  public void setAngle(Rotation2d angle)
-  {
-    rotateController.setSetpoint(angle.getRadians());
-  }
-  /**
-   * Tells whether the arm is extended
-   * @return whether the arm is extended
-   */
-  public boolean isExtended()
-  {
-    return Math.abs(getInnerExtendedArmLength() - Constants.ARM_INNER_EXTENDED_LENGTH) >= 0.05
-      && Math.abs(getOuterExtendedArmLength() - Constants.ARM_OUTER_EXTENDED_LENGTH) >= 0.05;
-  }
-  /**
-   * Tells whether the arm is retracted
-   * @return whether the arm is retracted
-   */
-  public boolean isRetracted()
-  {
-    return Math.abs(getInnerExtendedArmLength() - Constants.ARM_INNER_RETRACTED_LENGTH) >= 0.05
-      && Math.abs(getOuterExtendedArmLength() - Constants.ARM_OUTER_RETRACTED_LENGTH) >= 0.05;
-  }
-  /**
-   * Set arm angular speed
-   * @param speed between 1.0 and -1.0
-   */
-  public void setArmSpeed(double speed)
-  {
-    rotateController.setSetpoint(rotateController.getSetpoint() + Rotation2d.fromDegrees(speed).getRadians());
-  }
-  /**
-   * Gets the current translation of the end of the arm from the robot orign
-   * @return Translation3d
-   */
-  public Translation3d getArmTranslation()
-  {
-    return Constants.ARM_ORIGIN.plus(new Translation3d(
-      isExtended() ? Constants.ARM_EXTENDED_LENGTH : Constants.ARM_RETRACTED_LENGTH,
-      new Rotation3d(0, getAngle().getRadians(), 0)
-    ));
-  }
-  @Override
-  public void periodic() {
-    rightMotor.setVoltage(armFeedforward.calculate(getAngle().getRadians(), 0)
-      + rotateController.calculate(getAngle().getRadians()));
-    SmartDashboard.putNumber("Arm Angle", arm.getAngle().getDegrees());
   }
 }
