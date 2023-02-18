ef66eea80e308e16d624ecc94109d0d9a11c6318
FRCTeam172 <team172@northernforcerobotics.org>
2023-02-18 13:13:43 -0500

    added cg_mob_e

---
+++ src/main/java/frc/robot/commands/autoPaths/CG_Mob_E.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.autoPaths;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.autoComponents.DriveForMilliseconds;
+import frc.robot.commands.autoComponents.DriveMeters;
+import frc.robot.commands.autoComponents.PIDBalance;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class CG_Mob_E extends SequentialCommandGroup {
+  /** Creates a new CG_Mob_E. */
+  public CG_Mob_E() {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(
+      new DriveMeters(-0.3, 0, 0.3),
+      //Push piece of front
+      new DriveMeters(0.3, 0, 0.05),
+      new DriveMeters(0.55, 0, 0.05),
+      new DriveMeters(-0.55, 0, 0.2),
+      //Push piece forward
+      new DriveMeters(0.35, 0, 0.4),
+      new DriveForMilliseconds(.3, 0, 5500),
+
+      //Exit Community
+      new DriveMeters(-0.6, 0, 1),
+      new DriveMeters(-0.5, 0, 0.6),
+      new DriveMeters(-0.6, 0, 1),
+      // Reenter Charge Station
+      new DriveMeters(0.6, 0, 1.3),
+      //Stop
+      new PIDBalance()
+    );
+  }
+}
