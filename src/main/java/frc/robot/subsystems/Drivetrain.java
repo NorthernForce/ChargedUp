14b16069d97bec5b3c4492e5920768aa0eb18892
TrevinM <trevin.macomber@gmail.com>
2023-02-23 20:40:25 -0500

    Waiting for constants to be somewhere better

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -8,17 +8,17 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
-
-import static frc.robot.RobotContainer.activeChassis;
 
 public class Drivetrain extends SubsystemBase
 {
   private MotorGroup leftSide;
   private MotorGroup rightSide;
   private DifferentialDrive robotDrive;
+  private final DifferentialDriveKinematics kinematics = new DifferentialDriveKinematics(Constants.TRACK_WIDTH);
+  private final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(Constants.kS, Constants.kV, Constants.kA);
   private double speedProportion = 1.0, rotationSpeedProportion = 0.75;
   /** 
    * Creates a new Drivetrain. 
@@ -27,8 +27,6 @@ public class Drivetrain extends SubsystemBase
     this.leftSide = leftSide;
     this.rightSide = rightSide;
     robotDrive = new DifferentialDrive(leftSide, rightSide);
-    private final DifferentialDriveKinematics kinematics = new DifferentialDriveKinematics(activeChassis.TRACK_WIDTH);
-    private final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(Constants.kS, Constants.kV, Constants.kA);
   }
   /**
    * Drives the robot forward applying the speed proportions
@@ -124,6 +122,10 @@ public class Drivetrain extends SubsystemBase
     rightSide.setVoltage(right);
     robotDrive.feed();
   }
+  /**
+   * Drives using feet per second
+   * @param speeds
+   */
   public void driveUsingChassisSpeeds(ChassisSpeeds speeds) {
     DifferentialDriveWheelSpeeds driveSpeeds = kinematics.toWheelSpeeds(speeds);
     leftSide.setVoltage(feedforward.calculate(driveSpeeds.leftMetersPerSecond));
