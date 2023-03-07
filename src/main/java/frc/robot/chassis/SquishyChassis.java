2e02fe9dde74de05ad33917f225420470e76f6dd
cml1010101 <115354809+cml1010101@users.noreply.github.com>
2023-03-07 18:07:35 -0500

    Feature/#118 add constructor arguments to drivetrain so that drive constants can be chosen based off of robot (#120)
    * Created classes to control groups of motors
    
    * fixed some errors
    
    * fixed accidental commit conflict
    
    * Added ability for followers to oppose primary
    
    * Changed drivetrain construction to use MotorGroups
    
    * Trying to figure out how to move constant to chassis
    
    * Waiting for constants to be somewhere better
    
    * Flipped encoder outputs for inverted groups
    
    * Updted CAN IDs in the new robot code and worked in OI to make joystick inversions correct.
    
    * Added drivetrain returns into unfinished methods.
    
    * added construction parameters to Drivetrain and reflected those across the chassis
    
    * fixed typo
    
    ---------
    
    Co-authored-by: TrevinM <trevin.macomber@gmail.com>
    Co-authored-by: FRCTeam172 <team172@northernforcerobotics.org>

--- src/main/java/frc/robot/chassis/SquishyChassis.java
+++ src/main/java/frc/robot/chassis/SquishyChassis.java
@@ -6,18 +6,24 @@ package frc.robot.chassis;
 
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.util.Units;
+
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.MotorGroup;
 import frc.robot.subsystems.variants.MotorGroupSpark;
 
 /** Chassis specific details about squishy. */
 public class SquishyChassis implements ChassisBase {
+    public static final double TRACK_WIDTH = Units.inchesToMeters(24);
+    public static final double kS = 0.0;
+    public static final double kV = 0.0;
+    public static final double kA = 0.0;
     public SquishyChassis() {}
     public Drivetrain getDrivetrain() {
         MotorGroup left = new MotorGroupSpark(LEFT_PRIMARY_ID, new int[]{LEFT_FOLLOWER_ID}, MotorType.kBrushless);
         left.setInverted(true);
         MotorGroup right = new MotorGroupSpark(RIGHT_PRIMARY_ID, new int[]{RIGHT_FOLLOWER_ID}, MotorType.kBrushless);
-        return new Drivetrain(left, right);
+        return new Drivetrain(left, right, TRACK_WIDTH, kS, kV, kA);
     }
     public String getChassisName() {
         return "Squishy";
