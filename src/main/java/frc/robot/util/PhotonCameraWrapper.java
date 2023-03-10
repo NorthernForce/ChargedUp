53547b81d32b651635e4a4712e723aa753bd8725
cml1010101 <cml1010101@gmail.com>
2023-03-10 14:49:25 -0500

    reformed code to load from layout

--- src/main/java/frc/robot/util/PhotonCameraWrapper.java
+++ src/main/java/frc/robot/util/PhotonCameraWrapper.java
@@ -11,8 +11,6 @@ import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform3d;
-import frc.robot.Constants;
-import frc.robot.FieldConstants;
 
 public class PhotonCameraWrapper implements CameraWrapper {
     private final Transform3d transformToCenter;
