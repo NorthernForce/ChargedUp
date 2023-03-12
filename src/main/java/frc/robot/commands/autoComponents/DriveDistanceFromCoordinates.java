f9b4a750e607a21ba2a6f6bed36e146de45ac221
cml1010101 <cml1010101@gmail.com>
2023-03-11 22:11:33 -0500

    Created DriveDistanceToCoordinates... needs testing

---
+++ src/main/java/frc/robot/commands/autoComponents/DriveDistanceFromCoordinates.java
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.autoComponents;
+
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.wpilibj2.command.ProfiledPIDCommand;
+import frc.robot.Constants;
+
+import static frc.robot.RobotContainer.*;
+
+public class DriveDistanceFromCoordinates extends ProfiledPIDCommand {
+  /** Creates a new DriveDistanceFromCoordinates.
+   * Assumes aligned with coordinates.
+   */
+  public DriveDistanceFromCoordinates(double distance, Translation2d coordinates) {
+    super(
+      new ProfiledPIDController(
+        2e-1, 2e-2, 0, new Constraints(Constants.MAX_SPEED, Constants.MAX_ACCELERATION)
+      ),
+      () -> navigation.getPose2d().getTranslation().getDistance(coordinates),
+      distance,
+      (output, state) -> drivetrain.driveVolts(
+        -(output + drivetrain.getFeedforward().calculate(state.velocity)),
+        -(output + drivetrain.getFeedforward().calculate(state.velocity))
+      ),
+      navigation, drivetrain
+    );
+  }
+}
