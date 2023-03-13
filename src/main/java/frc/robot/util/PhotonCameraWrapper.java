64fbe25a4d7e18940fc367768f575cb57d89f09a
cml1010101 <115354809+cml1010101@users.noreply.github.com>
2023-03-13 17:29:21 -0400

    moved apriltag_layout to field constants & loaded from resource (#218)
    * moved apriltag_layout to field constants & loaded from resource
    
    * added static keyword to FieldConstants.java:376
    
    * fixed merge issues

--- src/main/java/frc/robot/util/PhotonCameraWrapper.java
+++ src/main/java/frc/robot/util/PhotonCameraWrapper.java
@@ -8,7 +8,6 @@ import org.photonvision.PhotonUtils;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform3d;
-import frc.robot.Constants;
 import frc.robot.FieldConstants;
 
 public class PhotonCameraWrapper implements CameraWrapper {
