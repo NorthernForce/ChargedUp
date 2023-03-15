543d9c300619d01defb3517080ef0c5310e25b58
FRCTeam172 <team172@northernforcerobotics.org>
2023-03-14 22:45:25 -0400

    Changed Pose3d to Transform3d

--- src/main/java/frc/robot/chassis/SquishyChassis.java
+++ src/main/java/frc/robot/chassis/SquishyChassis.java
@@ -8,6 +8,8 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
 import frc.lib.Motors.MotorGroup;
 import frc.lib.Motors.MotorGroupSpark;
@@ -40,27 +42,27 @@ public class SquishyChassis implements ChassisBase {
         public static final double LEFT_Y = Units.inchesToMeters(6.75);
         public static final double LEFT_Z = Units.inchesToMeters(10);
         public static final Rotation3d LEFT_ROTATION = new Rotation3d(0, 0, Units.degreesToRadians(60));
-        public static final Pose3d LEFT = new Pose3d(LEFT_X, LEFT_Y, LEFT_Z, LEFT_ROTATION);
+        public static final Transform3d LEFT = new Transform3d(new Translation3d(LEFT_X, LEFT_Y, LEFT_Z), LEFT_ROTATION);
 
         public static final String LEFT_FORWARD_NAME = "LeftForward";
         public static final double LEFT_FORWARD_X = Units.inchesToMeters(4);
         public static final double LEFT_FORWARD_Y = Units.inchesToMeters(6);
         public static final double LEFT_FORWARD_Z = Units.inchesToMeters(7);
         public static final Rotation3d LEFT_FORWARD_ROTATION = new Rotation3d(0, 0, 0);
-        public static final Pose3d LEFT_FORWARD = new Pose3d(LEFT_FORWARD_X, LEFT_FORWARD_Y, LEFT_FORWARD_Z, LEFT_FORWARD_ROTATION);
+        public static final Transform3d LEFT_FORWARD = new Transform3d(new Translation3d(LEFT_FORWARD_X, LEFT_FORWARD_Y, LEFT_FORWARD_Z), LEFT_FORWARD_ROTATION);
     
         public static final String RIGHT_NAME = "Right";
         public static final double RIGHT_X = Units.inchesToMeters(1.5);
         public static final double RIGHT_Y = Units.inchesToMeters(-6.5);
         public static final double RIGHT_Z = Units.inchesToMeters(10.0871);
         public static final Rotation3d RIGHT_ROTATION = new Rotation3d(0, 0, Units.degreesToRadians(-60));
-        public static final Pose3d RIGHT = new Pose3d(RIGHT_X, RIGHT_Y, RIGHT_Z, RIGHT_ROTATION);
+        public static final Transform3d RIGHT = new Transform3d(new Translation3d(RIGHT_X, RIGHT_Y, RIGHT_Z), RIGHT_ROTATION);
     
         public static final String LIMELIGHT_NAME = "Limelight";
         public static final double LIMELIGHT_X = Units.inchesToMeters(5.5);
         public static final double LIMELIGHT_Y = Units.inchesToMeters(-5);
         public static final double LIMELIGHT_Z = Units.inchesToMeters(7);
         public static final Rotation3d LIMELIGHT_ROTATION = new Rotation3d(0, 0, 0);
-        public static final Pose3d limeLight = new Pose3d(LIMELIGHT_X, LIMELIGHT_Y, LIMELIGHT_Z, LIMELIGHT_ROTATION);
+        public static final Transform3d limeLight = new Transform3d(new Translation3d(LIMELIGHT_X, LIMELIGHT_Y, LIMELIGHT_Z), LIMELIGHT_ROTATION);
     }
 }
