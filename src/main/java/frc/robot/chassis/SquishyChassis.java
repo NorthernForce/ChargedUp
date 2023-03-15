1f052c4d2a7d7ab46497d8496e9567aedf336d1f
FRCTeam172 <team172@northernforcerobotics.org>
2023-03-14 22:14:12 -0400

    Added constants to the squishy chassis outlining camera locations.

--- src/main/java/frc/robot/chassis/SquishyChassis.java
+++ src/main/java/frc/robot/chassis/SquishyChassis.java
@@ -6,6 +6,8 @@ package frc.robot.chassis;
 
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.util.Units;
 import frc.lib.Motors.MotorGroup;
 import frc.lib.Motors.MotorGroupSpark;
@@ -20,9 +22,8 @@ public class SquishyChassis implements ChassisBase {
     public SquishyChassis() {}
     public Drivetrain getDrivetrain() {
         MotorGroup left = new MotorGroupSpark(MotorType.kBrushless, LEFT_PRIMARY_ID, LEFT_FOLLOWER_ID);
-        left.setInverted(true);
-
-        MotorGroup right = new MotorGroupSpark(MotorType.kBrushless, RIGHT_PRIMARY_ID, new int[]{RIGHT_FOLLOWER_ID});
+        MotorGroup right = new MotorGroupSpark(MotorType.kBrushless, RIGHT_PRIMARY_ID, RIGHT_FOLLOWER_ID);
+        right.setInverted(true);
         return new Drivetrain(left, right, TRACK_WIDTH, kS, kV, kA);
 
     }
@@ -33,4 +34,10 @@ public class SquishyChassis implements ChassisBase {
     public static final int RIGHT_PRIMARY_ID = 2;
     public static final int LEFT_FOLLOWER_ID = 3;
     public static final int RIGHT_FOLLOWER_ID = 4;
+    public static class CameraLocations {
+        public static final Pose3d left = new Pose3d(Units.inchesToMeters(1.5), Units.inchesToMeters(6.75), Units.inchesToMeters(10), new Rotation3d(0, 0, Units.degreesToRadians(60)));
+        public static final Pose3d leftForward = new Pose3d(Units.inchesToMeters(4), Units.inchesToMeters(6), Units.inchesToMeters(7), new Rotation3d(0, 0, 0));
+        public static final Pose3d right = new Pose3d(Units.inchesToMeters(1.5), Units.inchesToMeters(-6.5), Units.inchesToMeters(10.0871), new Rotation3d(0, 0, Units.degreesToRadians(-60)));
+        public static final Pose3d limeLight = new Pose3d(Units.inchesToMeters(5.5), Units.inchesToMeters(-5), Units.inchesToMeters(7), new Rotation3d(0, 0, 0));
+    }
 }
