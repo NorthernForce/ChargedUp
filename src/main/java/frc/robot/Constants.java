5ff3e59121aaef2f3a7c4149f7bdd4dda4f4530c
cml1010101 <cml1010101@gmail.com>
2023-03-15 16:24:59 -0400

    forgot to log some stuff

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -150,8 +150,8 @@ public final class Constants {
             Units.inchesToMeters(0),
             Units.inchesToMeters(26.5)
         );
-        public static final double kP = 0.0; // TODO
-        public static final double kF = 0.1; // TODO
+        public static final double kP = 2.0; // TODO
+        public static final double kF = 0.0; // TODO
         public static final double kI = 0.0; // TODO
         public static final double kD = 0.0; // TODO
         public static final double kFF = 0.0; // TODO
