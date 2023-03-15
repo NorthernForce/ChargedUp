5cd65132f12792675dd3ead6b39d4038185fe36b
FRCTeam172 <team172@northernforcerobotics.org>
2023-03-14 22:40:39 -0400

    Changed definitions to use constants

--- src/main/java/frc/robot/chassis/SquishyChassis.java
+++ src/main/java/frc/robot/chassis/SquishyChassis.java
@@ -35,9 +35,32 @@ public class SquishyChassis implements ChassisBase {
     public static final int LEFT_FOLLOWER_ID = 3;
     public static final int RIGHT_FOLLOWER_ID = 4;
     public static class CameraLocations {
-        public static final Pose3d left = new Pose3d(Units.inchesToMeters(1.5), Units.inchesToMeters(6.75), Units.inchesToMeters(10), new Rotation3d(0, 0, Units.degreesToRadians(60)));
-        public static final Pose3d leftForward = new Pose3d(Units.inchesToMeters(4), Units.inchesToMeters(6), Units.inchesToMeters(7), new Rotation3d(0, 0, 0));
-        public static final Pose3d right = new Pose3d(Units.inchesToMeters(1.5), Units.inchesToMeters(-6.5), Units.inchesToMeters(10.0871), new Rotation3d(0, 0, Units.degreesToRadians(-60)));
-        public static final Pose3d limeLight = new Pose3d(Units.inchesToMeters(5.5), Units.inchesToMeters(-5), Units.inchesToMeters(7), new Rotation3d(0, 0, 0));
+        public static final String LEFT_NAME = "Left";
+        public static final double LEFT_X = Units.inchesToMeters(1.5);
+        public static final double LEFT_Y = Units.inchesToMeters(6.75);
+        public static final double LEFT_Z = Units.inchesToMeters(10);
+        public static final Rotation3d LEFT_ROTATION = new Rotation3d(0, 0, Units.degreesToRadians(60));
+        public static final Pose3d LEFT = new Pose3d(LEFT_X, LEFT_Y, LEFT_Z, LEFT_ROTATION);
+
+        public static final String LEFT_FORWARD_NAME = "LeftForward";
+        public static final double LEFT_FORWARD_X = Units.inchesToMeters(4);
+        public static final double LEFT_FORWARD_Y = Units.inchesToMeters(6);
+        public static final double LEFT_FORWARD_Z = Units.inchesToMeters(7);
+        public static final Rotation3d LEFT_FORWARD_ROTATION = new Rotation3d(0, 0, 0);
+        public static final Pose3d LEFT_FORWARD = new Pose3d(LEFT_FORWARD_X, LEFT_FORWARD_Y, LEFT_FORWARD_Z, LEFT_FORWARD_ROTATION);
+    
+        public static final String RIGHT_NAME = "Right";
+        public static final double RIGHT_X = Units.inchesToMeters(1.5);
+        public static final double RIGHT_Y = Units.inchesToMeters(-6.5);
+        public static final double RIGHT_Z = Units.inchesToMeters(10.0871);
+        public static final Rotation3d RIGHT_ROTATION = new Rotation3d(0, 0, Units.degreesToRadians(-60));
+        public static final Pose3d RIGHT = new Pose3d(RIGHT_X, RIGHT_Y, RIGHT_Z, RIGHT_ROTATION);
+    
+        public static final String LIMELIGHT_NAME = "Limelight";
+        public static final double LIMELIGHT_X = Units.inchesToMeters(5.5);
+        public static final double LIMELIGHT_Y = Units.inchesToMeters(-5);
+        public static final double LIMELIGHT_Z = Units.inchesToMeters(7);
+        public static final Rotation3d LIMELIGHT_ROTATION = new Rotation3d(0, 0, 0);
+        public static final Pose3d limeLight = new Pose3d(LIMELIGHT_X, LIMELIGHT_Y, LIMELIGHT_Z, LIMELIGHT_ROTATION);
     }
 }
