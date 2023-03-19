87c52f871fe566b8f235d9f15b31eaaba484cc3e
FRCTeam172 <team172@northernforcerobotics.org>
2023-03-19 14:53:22 -0400

    changes to autos: final

--- src/main/java/frc/robot/commands/autoPaths/CG_Mob_E.java
+++ src/main/java/frc/robot/commands/autoPaths/CG_Mob_E.java
@@ -29,12 +29,8 @@ public class CG_Mob_E extends SequentialCommandGroup {
       //Push piece forward
       new DriveMeters(0.5, 0, 0.1),
       //Exit Community
-      new DriveMeters(-0.7, 0, 0.6),
+      new DriveMeters(-0.6, 0, 0.6),
       new DriveMeters(-0.6, 0, 1.5),
-      new DriveMeters(-0.7, 0, 1.8),
-      new Stop(0.1),
-      new WaitCommand(0.4),
-      new DriveMeters(0.85, 0, 2),
       // Reenter Charge Station
       new Stop(0.05),
       new Balance()
