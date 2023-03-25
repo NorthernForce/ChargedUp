9bccbff724d50c6cf1d9980f23ddaf54dfc44001
FRCTeam172 <team172@northernforcerobotics.org>
2023-03-25 09:56:10 -0400

    FIXED CENTER PATHS

--- src/main/java/frc/robot/commands/autoPaths/RedCenter.java
+++ src/main/java/frc/robot/commands/autoPaths/RedCenter.java
@@ -6,6 +6,7 @@ package frc.robot.commands.autoPaths;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
@@ -36,10 +37,10 @@ public class RedCenter extends SequentialCommandGroup {
       new RetractArm(),
       new SetArmAngle(Rotation2d.fromDegrees(60)),
       new DriveMeters(-0.55, 0, 1.5),
-      new SetArmAngle(Rotation2d.fromDegrees(180)),
-      new DriveMeters(-0.6, 0, 0.6),
-      new SetArmAngle(Rotation2d.fromDegrees(90)),
-      new Balance()
+      new ParallelCommandGroup(new SetArmAngle(Rotation2d.fromDegrees(180)),
+      new DriveMeters(-0.6, 0, 0.6)),
+      new ParallelCommandGroup(new SetArmAngle(Rotation2d.fromDegrees(90)),
+      new Balance())
     );
   }
 }
