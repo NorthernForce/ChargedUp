b54cd1a883885b27f367f306999c21010d064d01
FRCTeam172 <team172@northernforcerobotics.org>
2023-03-26 10:42:11 -0400

    fixed deploy issue

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -83,7 +83,6 @@ public class RobotContainer {
     autonomousChooser.addOption("Red Center Balance Only", new RedOnlyCenter());
     autonomousChooser.addOption("Blue Center Balance Only", new BlueOnlyCenter());
     autonomousChooser.setDefaultOption("Just score high", new PlaceHigh());
-    autonomousChooser.addOption("Red Mob", getAutonomousCommand());
     //autonomousChooser.addOption("Temporary Test", new FoldWristBack());
     /**
      * An IOException occurs when you access files that cause errors of a sort.
