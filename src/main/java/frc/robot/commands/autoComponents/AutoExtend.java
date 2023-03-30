e6cee29ce4b4ca25f140495bcc87ca582c031a67
cml1010101 <cml1010101@gmail.com>
2023-03-30 13:02:04 -0400

    requires that arm wait to be extended

---
+++ src/main/java/frc/robot/commands/autoComponents/AutoExtend.java
@@ -0,0 +1,22 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.autoComponents;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.ExtendArm;
+
+import static frc.robot.RobotContainer.*;
+
+public class AutoExtend extends SequentialCommandGroup {
+  /** Creates a new AutoExtend. */
+  public AutoExtend() {
+    addCommands(
+      Commands.deadline(Commands.waitUntil(() -> armTelescope.isExtended()), Commands.repeatingSequence(new ExtendArm())),
+      Commands.waitSeconds(1.5)
+    );
+  }
+}
