5bc5f854253c89ba91b1de28afb43798d9567d4c
cml1010101 <cml1010101@gmail.com>
2023-03-31 11:56:23 -0400

    checks to see whether camera estimation is reasonable

--- src/main/java/frc/lib/cameras/PhotonCameraWrapper.java
+++ src/main/java/frc/lib/cameras/PhotonCameraWrapper.java
@@ -9,16 +9,19 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform3d;
 import frc.robot.FieldConstants;
+import frc.robot.Constants.DrivetrainConstants;
 
 public class PhotonCameraWrapper implements CameraWrapper {
     private final Transform3d transformToCenter;
     private final PhotonPoseEstimator visionEstimator;
+    private EstimatedRobotPose lastEstimatedRobotPose;
     private final PhotonCamera camera;
     public PhotonCameraWrapper(String name, Transform3d transform)
     {
         camera = new PhotonCamera(name);
         transformToCenter = transform;
         visionEstimator = new PhotonPoseEstimator(FieldConstants.APRILTAG_LAYOUT, PhotonPoseEstimator.PoseStrategy.MULTI_TAG_PNP, camera, transform);
+        lastEstimatedRobotPose = null;
     }
     /**
      * Returns the Transform3d (X,Y,Z comp and Pitch, Roll, Yaw comp)
@@ -91,7 +94,28 @@ public class PhotonCameraWrapper implements CameraWrapper {
     {
         visionEstimator.setReferencePose(referencePose);
         var results = visionEstimator.update();
-        if (results.isPresent()) return results.get();
+        if (lastEstimatedRobotPose != null && results.isPresent())
+        {
+            double dtime = results.get().timestampSeconds - lastEstimatedRobotPose.timestampSeconds;
+            double distance = results.get().estimatedPose.toPose2d().getTranslation().getDistance(
+                lastEstimatedRobotPose.estimatedPose.toPose2d().getTranslation()
+            );
+            double speed = distance / dtime;
+            if (speed <= DrivetrainConstants.MAX_POSSIBLE_SPEED)
+            {
+                lastEstimatedRobotPose = results.get();
+                return lastEstimatedRobotPose;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        else if (results.isPresent())
+        {
+            lastEstimatedRobotPose = results.get();
+            return lastEstimatedRobotPose;
+        }
         else return null;
     }
 }
