bc4663d4433b130060f5028412834dd6dcb8e786
cml1010101 <cml1010101@gmail.com>
2023-04-02 13:21:17 -0400

    changes to make wrist stuff work

--- src/main/java/frc/robot/commands/SetWristAngle.java
+++ src/main/java/frc/robot/commands/SetWristAngle.java
@@ -38,7 +38,9 @@ public class SetWristAngle extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    wrist.setPercent(0);
+  }
 
   // Returns true when the command should end.
   @Override
