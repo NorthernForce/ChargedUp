a763557ba0a7c7b05689c01f1ae7618019eb5506
FRCTeam172 <125146008+FRCTeam172@users.noreply.github.com>
2023-04-03 22:16:03 -0400

    Feature/#292 drivers tab (#293)
    * Started organizing the Drivers tab
    
    * Add manipulator color to dashboard.
    
    * Properly format the drivers tab

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -187,8 +187,8 @@ public class RobotContainer {
     startingLocationChooser.addOption("Blue Left", FieldConstants.BLUE_POSES[0]);
     startingLocationChooser.addOption("Blue Center", FieldConstants.BLUE_POSES[1]);
     startingLocationChooser.addOption("Blue Right", FieldConstants.BLUE_POSES[2]);
-    Shuffleboard.getTab("Autonomous").add("Autonomous Routine Chooser", autonomousChooser).withSize(2, 1).withPosition(2, 2);
-    Shuffleboard.getTab("Autonomous").add("Starting Location Chooser", startingLocationChooser).withSize(2, 1).withPosition(0, 2);
+    Shuffleboard.getTab("Drivers").add("Autonomous Routine Chooser", autonomousChooser).withSize(2, 1).withPosition(2, 3);
+    Shuffleboard.getTab("Drivers").add("Starting Location Chooser", startingLocationChooser).withSize(2, 1).withPosition(0, 3);
     Shuffleboard.getTab("Utility").add("Calibrate IMU", new CalibrateIMU()).withPosition(0, 0);
     Shuffleboard.getTab("Utility").add("Stop", new Stop(0.1)).withPosition(1, 0);
     Shuffleboard.getTab("Utility").add("Balance", new Balance()).withPosition(2, 0);
@@ -197,7 +197,7 @@ public class RobotContainer {
     Shuffleboard.getTab("Utility").add("Calibrate Wrist", new CalibrateWrist());
     Shuffleboard.getTab("Arm").add("Calibrate Arm", new CalibrateArm());
     Shuffleboard.getTab("Drivers").addBoolean("Manipulating State", () -> manipulatingState.getCurrentState().getClass().equals(CubeManipulatingState.class))
-      .withPosition(5, 1)
+      .withPosition(4, 0).withSize(1, 4)
       .withProperties(Map.of("colorWhenTrue", "purple", "colorWhenFalse", "yellow"));
   }
   /**
