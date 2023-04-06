032a08a6d4318156b23153308097e96c4bd5a1c2
FRCTeam172 <team172@northernforcerobotics.org>
2023-04-06 18:43:11 -0400

    maybe works

--- src/main/java/frc/robot/commands/autoPaths/RedCenterMobilityBalance.java
+++ src/main/java/frc/robot/commands/autoPaths/RedCenterMobilityBalance.java
@@ -39,7 +39,7 @@ public class RedCenterMobilityBalance extends SequentialCommandGroup {
       new DriveMeters(-0.7, 0, 2.5),
       new DriveMeters(-0.5, 0, 2.6),
       new ParallelCommandGroup(new SetArmAngle(Rotation2d.fromDegrees(180)),
-      new DriveMeters(0.65, 0, 2.8)),
+      new DriveMeters(0.7, 0, 2.8)),
       new ParallelCommandGroup(new SetArmAngle(Rotation2d.fromDegrees(90)),
       new Balance())
     );
