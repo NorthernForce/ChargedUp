d8154cea97246ea02d479eb9482411b7633b947c
Harlan Haller <64970252+HarlanHaller@users.noreply.github.com>
2023-06-18 14:02:19 -0400

    fix the mobility balance center autos (#333)
    * tuned the mobility centers
    
    * maybe works
    
    * Adjusted center mobility to climb after mobility
    
    ---------
    
    Co-authored-by: FRCTeam172 <team172@northernforcerobotics.org>

--- src/main/java/frc/robot/commands/autoPaths/RedCenterMobilityBalance.java
+++ src/main/java/frc/robot/commands/autoPaths/RedCenterMobilityBalance.java
@@ -31,15 +31,16 @@ public class RedCenterMobilityBalance extends SequentialCommandGroup {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
+      
       //new FoldWristBack(),
       new PositionWithTarget(FieldConstants.RED_CUBE_PLACEMENT_LOCATIONS[3].toTranslation2d(), AnglesAndDistances.HIGH_CUBE.getFirst(), AnglesAndDistances.HIGH_CUBE.getSecond(), Rotation2d.fromDegrees(10), true),
       new ParallelDeadlineGroup(new WaitCommand(0.8), new Outtake()),
       new RetractArm(),
       new ParallelCommandGroup(new SetArmAngle(Rotation2d.fromDegrees(30))),
       new DriveMeters(-0.7, 0, 2.5),
-      new DriveMeters(-0.5, 0, 2),
+      new DriveMeters(-0.5, 0, 2.6),
       new ParallelCommandGroup(new SetArmAngle(Rotation2d.fromDegrees(180)),
-      new DriveMeters(0.65, 0, 2.7)),
+      new DriveMeters(0.55, 0, 2.4)),
       new ParallelCommandGroup(new SetArmAngle(Rotation2d.fromDegrees(90)),
       new Balance())
     );
